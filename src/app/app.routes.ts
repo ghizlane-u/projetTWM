95f5c6d1793570f4c90cf34467938493a1ef7a84
hiba-s25 <hibasalam254@gmail.com>
2025-01-29 00:45:39 +0100

    Message pour tes modifications

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 import { ContactComponent } from './contact/contact.component';
 import { ColddrinksComponent } from './colddrinks/colddrinks.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   { path: 'signin', component: SigninComponent },
   { path: 'cont', component: ContactComponent },
-  { path: 'cold', component: ColddrinksComponent }
+  { path: 'cold', component: ColddrinksComponent },
+  {path:'about',component:AboutComponent},
 ];
