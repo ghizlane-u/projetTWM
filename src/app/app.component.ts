c98853254e443d283357626a134380da400c2a84
hiba-s25 <hibasalam254@gmail.com>
2025-01-29 18:23:25 +0100

    Sauvegarde de mes modifications avant de changer de branche

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet,RouterModule } from '@angular/router';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ContactComponent } from './contact/contact.component';
 import { ColddrinksComponent } from './colddrinks/colddrinks.component';
 
 import { CommonModule } from '@angular/common'; // Importer CommonModule
 import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './about/about.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,  // Marquez ce composant comme autonome
-  imports: [RouterOutlet, RouterLink, HeaderComponent, GalleryComponent,  CommonModule,ContactComponent,ColddrinksComponent,AboutComponent]
+  imports: [RouterModule,RouterOutlet, RouterLink, HeaderComponent, GalleryComponent,  CommonModule,ContactComponent,ColddrinksComponent]
 })
 export class AppComponent {
   title = 'angular-app';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,14 @@ import { ContactComponent } from './contact/contact.component';
 import { ColddrinksComponent } from './colddrinks/colddrinks.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { AboutComponent } from './about/about.component';
-
+import { AppComponent } from './app.component';
 
 
 export const routes: Routes = [
- { path: '', component: GalleryComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirige vers 'home' au chargement
+  { path: 'home', component: GalleryComponent }, // Supposons que GalleryComponent est la page d'accueil
   { path: 'about', component: AboutComponent },
   { path: 'cont', component: ContactComponent },
   { path: 'cold', component: ColddrinksComponent },
-  { path: '**', redirectTo: '' }
-
-]
+  { path: '**', redirectTo: 'home', pathMatch: 'full' } // Redirige les routes inconnues
+];
