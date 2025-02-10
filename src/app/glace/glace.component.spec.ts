9d0d1d9d03d695d8cf6f73edf5fd13a319b09ecf
hiba-s25 <hibasalam254@gmail.com>
2025-02-10 15:11:43 +0100

    Résolution du conflit dans glace.component.spec.ts

--- src/app/glace/glace.component.spec.ts
+++ src/app/glace/glace.component.spec.ts
@@ -1,20 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-<<<<<<<< HEAD:src/app/footer/footer.component.spec.ts
-import { FooterComponent } from './footer.component';
-
-describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [FooterComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(FooterComponent);
-========
 import { GlaceComponent } from './glace.component';
 
 describe('GlaceComponent', () => {
@@ -28,7 +12,6 @@ describe('GlaceComponent', () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(GlaceComponent);
->>>>>>>> c71715bb9fe1638fcdf35a5936b80aa87b7e265b:src/app/glace/glace.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
